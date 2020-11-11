20ce30643b74a277ff17a5a303ecc733578de6b2
Alejandro Vargas <alejandrovargas@Alejandros-MacBook-Pro.local>
2020-11-11 13:28:09 -0800

    server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require ('express');
+
+const app = express();
+
+const bodyParser = require ('body-parser')
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+const cors = require ('cors');
+app.use(cors());
+
+app.use(express.static('/'));
+
+const port = 8000;
+
+const server = app.listen(port, () => {
+    console.log(`running in port : ${port}`);
+});
